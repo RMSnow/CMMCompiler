26cfb3c5c034920e0b3c024bc266fded972161ea
snow <xueyao_98@foxmail.com>
2017-11-16 16:26:51 +0800

    Demo: Get Started

---
+++ src/demo/main/Main.java
@@ -0,0 +1,14 @@
+package demo.main;
+
+/**
+ * 程序入口
+ */
+public class Main {
+    /**
+     * main创建了一个词法分析器和一个语法分析器，然后调用语法分析器中的方法program
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
+}
